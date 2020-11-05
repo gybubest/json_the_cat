00fa84fa56dec8cc050756dfb4d507931cd3d86d
Yu Gao <gaoyububest@gmail.com>
2020-11-05 20:31:48 +0000

    test case modified

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,15 +3,16 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
   const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
   request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+
     if (error) {
       callback(error, null);
-    } else  {
-      const data = JSON.parse(body);
-      if (!data[0]) {
-        callback(null, 'Breed not found.');
-      } else {
-        callback(null, data[0]['description']);
-      }
+      return;
+    } else if (!data[0]) {
+      callback(Error('Breed not found.'), null);
+      return;
+    } else {
+      callback(null, data[0]['description']);
     }
   });
 };
--- test/breedFetcherTest.js
+++ test/breedFetcherTest.js
@@ -15,10 +15,7 @@ describe('fetchBreedDescription', () => {
 
   it('returns a string description for a valid breed, via callback', (done) => {
     fetchBreedDescription('nonexistent', (err, desc) => {
-      assert.equal(err, null);
-
-      const expectedDesc = 'Breed not found.';
-      assert.equal(expectedDesc, desc.trim());
+      assert.equal(desc, null);
 
       done();
     });
